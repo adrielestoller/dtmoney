d6c10a0821a8b4e9bc85cf2220026a3369d6befc
Adriel Estoller <adrielestoller@nave.org.br>
2021-11-04 20:18:29 -0300

    initial commit

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+import Summary from "../Summary";
+import TransactionsTable from "../TransactionsTable";
+
+function Dashboard(){
+    return(
+        <Container>
+            <Summary />
+            <TransactionsTable />
+        </Container>
+    );
+}
+
+export default Dashboard;
